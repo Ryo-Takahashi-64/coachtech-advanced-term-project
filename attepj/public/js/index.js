8418ab69049adc53dd12a0c3010a182c0faa2082
Ryo Takahashi <ryo.takahashi.tr0105@gmail.com>
2022-01-17 01:25:11 +0900

    20220117 修正版アップロード

--- attepj/public/js/index.js
+++
@@ -1,7 +0,0 @@
-// ヘッダ　ハンバーガーメニュー開閉
-const target = document.getElementById('menu');
-target.addEventListener('click', () => {
-  target.classList.toggle('open');
-  const nav = document.getElementById('nav');
-  nav.classList.toggle('in');
-});
--- attepj/resources/views/components/button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-blue-700 rounded-md font-semibold text-xs text-white uppercase tracking-widest disabled:opacity-25 transition duration-150 hover:bg-blue-700 w-full mt-6 justify-center focus:outline-none']) }}>
-    {{ $slot }}
-</button>
